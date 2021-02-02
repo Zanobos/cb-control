ff6ecb532ab63b728fe0d3e329e2062c6edf619c
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2021-02-02 02:15:44 +0100

    Implementata paginazione in query admin

--- control_node/cbdash-fe/src/pages/ChargeGraph.vue
+++ control_node/cbdash-fe/src/pages/ChargeGraph.vue
@@ -139,7 +139,8 @@ import { mapState } from 'vuex'
 
 const queryApi = new InfluxDB({url, token}).getQueryApi(org)
 
-const maxDataPoints = 1000
+// Assuming 1 datapoint per ms
+const maxDataPoints = 10000
 const currentDomain = [-500, 200]
 const voltageDomain = [0, 100]
 const temperatureDomain = [-20, 70]
@@ -170,6 +171,50 @@ const baseGiraffeLineLayerConfig = {
   shadeBelowOpacity: 0.1
 }
 
+// DO NOT SHARE CONFIG OBJECTS
+
+const timeFormatter = Giraffe.timeFormatter({
+      timeZone: 'UTC',
+      format: 'YYYY-MM-DD HH:mm:ss ZZ',
+    })
+
+const currentConfig = { 
+    layers: [{
+      ...baseGiraffeLineLayerConfig,
+      maxTooltipRows: 120
+    }],
+    valueFormatters: {
+      _time: timeFormatter,
+      _value: (num) => num.toFixed(1)+' A'
+    },
+    legendColumns: ["_time", "_value"],
+    yDomain: currentDomain,
+    ...commonGiraffeConfigs
+  };
+
+const voltageConfig = { 
+    layers: [{
+      ...baseGiraffeLineLayerConfig,
+      colors: ['orange']
+    }],
+    valueFormatters: {
+      _time: timeFormatter,
+      _value: (num) => num.toFixed(1)+' V'
+    },
+    yDomain: voltageDomain,
+    ...commonGiraffeConfigs
+  };
+
+const temperatureConfig = { 
+    layers: [ baseGiraffeLineLayerConfig ],
+    valueFormatters: {
+      _time: timeFormatter,
+      _value: (num) => num.toFixed(1)+' °C'
+    },
+    yDomain: temperatureDomain,
+    ...commonGiraffeConfigs
+  };  
+
 export default {
     components: {
         Card
@@ -178,6 +223,7 @@ export default {
     return {
       timer: null,
       selectedBMS: '',
+      loadedBMS: '',
       loadedData: {
         current: { loadedFlag: false, loadedBMS: '' },
         voltage: { loadedFlag: false, loadedBMS: '' },
@@ -188,6 +234,9 @@ export default {
         start: '',
         end: '',
       },
+      currentRange: {},
+      voltageRange: {},
+      temperatureRange: {},
       masks: {
         input: 'YYYY-MM-DD h:mm A',
       },
@@ -217,7 +266,6 @@ export default {
     });
     this.whiteTheme = document.body.classList.contains('white-content');
     this.toggleChartTheme()
-    //this.loadData()
   },
   methods: {
     queryToCsv(query, fileName) {
@@ -279,6 +327,8 @@ export default {
 
       var timeRangeMs = endDateTime - startDateTime
       var aggregationIntervalMs = Math.floor(timeRangeMs / maxDataPoints)
+
+      if(aggregationIntervalMs < 1) aggregationIntervalMs = 1
 
       if(aggregationIntervalMs < second)
         return aggregationIntervalMs+'ms'
@@ -344,7 +394,7 @@ export default {
 
         return giraffeConfig
     },
-    queryAndRender(query, bms, giraffeConfig, serieName, domId, loadedFlagPointer, threshold = null) {
+    queryAndRender(query, bms, giraffeConfig, seriesName, domId, loadedFlagPointer, threshold = null) {
       var outerScope = this
       queryToTable(
         queryApi,
@@ -352,17 +402,23 @@ export default {
         Giraffe.newTable
       ). then(table => {
         console.log("Datapoint fetched: " + table.length)
-
-        if(table.length) {
-          table.columns._time.name = 'Time'
-          table.columns._value.name = serieName
-        }
-        
+          
         if(threshold != null && table.length) {
           giraffeConfig = outerScope.segmentTable(table, giraffeConfig, threshold)
         }
         else{
           giraffeConfig.table = table
+        }
+
+        if(table.length) {
+          table.columns._time.name = 'Time'
+          table.columns._value.name = seriesName
+          
+          // Force the graph to zoom between first and last available data
+          const timeValues = table.columns._time.data
+          const firstDataPointTime = timeValues[0]
+          const lastDataPointTime = timeValues[timeValues.length - 1]
+          giraffeConfig.xDomain = [firstDataPointTime, lastDataPointTime]          
         }
         ReactDOM.render(       
             outerScope.renderGiraffePlot(giraffeConfig),
@@ -381,92 +437,94 @@ export default {
       })
     },
     loadData() {
+      this.loadedBMS = this.selectedBMS
 
       this.loadedData.current.loadedFlag = false
       this.loadedData.voltage.loadedFlag = false
       this.loadedData.temperature.loadedFlag = false
 
-      // DO NOT SHARE CONFIG OBJECTS
+      this.currentRange = {...this.range}
+      this.voltageDomain = {...this.range}
+      this.temperatureRange = {...this.range}
 
-      const timeFormatter = Giraffe.timeFormatter({
-            timeZone: 'UTC',
-            format: 'YYYY-MM-DD HH:mm:ss ZZ',
-          })
+      if(this.logged) {
+        this.setGiraffeControlledMode()
+      } else {
+        this.setGiraffeDefaultMode()
+      }
 
-      const currentConfig = { 
-          layers: [{
-            ...baseGiraffeLineLayerConfig,
-            maxTooltipRows: 120
-          }],
-          valueFormatters: {
-            _time: timeFormatter,
-            _value: (num) => num.toFixed(1)+' A'
-          },
-          legendColumns: ["_time", "_value"],
-          yDomain: currentDomain,
-          //xDomain: [this.range.start.getTime(), this.range.end.getTime()],
-          //onSetXDomain: () => {},
-          //onResetXDomain: () => {},
-          ...commonGiraffeConfigs
-        };
+      this.queryAndRender(this.buildQuery('current', this.currentRange), this.selectedBMS, currentConfig, 'Current', 'chart-current', this.loadedData.current, 0)
+      this.queryAndRender(this.buildQuery('voltage', this.voltageDomain), this.selectedBMS, voltageConfig, 'Voltage', 'chart-voltage', this.loadedData.voltage)
+      this.queryAndRender(this.buildQuery('tempBatt', this.temperatureRange), this.selectedBMS, temperatureConfig, 'Temperature', 'chart-temperature', this.loadedData.temperature)
 
-      const voltageConfig = { 
-          layers: [{
-            ...baseGiraffeLineLayerConfig,
-            colors: ['orange']
-          }],
-          valueFormatters: {
-            _time: timeFormatter,
-            _value: (num) => num.toFixed(1)+' V'
-          },
-          yDomain: voltageDomain,
-          ...commonGiraffeConfigs
-        };
+    },
+    setGiraffeDefaultMode() {
+      currentConfig.xDomain = null
+      currentConfig.onSetXDomain = null
+      currentConfig.onResetXDomain = null
+      voltageConfig.xDomain = null
+      voltageConfig.onSetXDomain = null
+      voltageConfig.onResetXDomain = null
+      temperatureConfig.xDomain = null
+      temperatureConfig.onSetXDomain = null
+      temperatureConfig.onResetXDomain = null
+    },
+    setGiraffeControlledMode() {
+      // If the 3 config parameters xDomain, onSetXDomain, onResetXDomain are all set, giraffe works in "controlled mode"
+      
+      currentConfig.xDomain = null
+      //currentConfig.xDomain = [this.range.start.getTime(), this.range.end.getTime()]
+      currentConfig.onSetXDomain = (range) => {
+        const newRange = {start: new Date(range[0]), end: new Date(range[1])}
+        currentConfig.xDomain = [newRange.start.getTime(), newRange.end.getTime()]
+        this.queryAndRender(this.buildQuery('current', newRange), this.loadedBMS, currentConfig, 'Current', 'chart-current', this.loadedData.current, 0)
+      }
+      currentConfig.onResetXDomain = () => {
+        currentConfig.xDomain = [this.range.start.getTime(), this.range.end.getTime()]
+        this.queryAndRender(this.buildQuery('current', this.range), this.loadedBMS, currentConfig, 'Current', 'chart-current', this.loadedData.current, 0)
+      }
 
-      const temperatureConfig = { 
-          layers: [ baseGiraffeLineLayerConfig ],
-          valueFormatters: {
-            _time: timeFormatter,
-            _value: (num) => num.toFixed(1)+' °C'
-          },
-          yDomain: temperatureDomain,
-          ...commonGiraffeConfigs
-        };   
+      voltageConfig.xDomain = null
+      //voltageConfig.xDomain = [this.range.start.getTime(), this.range.end.getTime()]
+      voltageConfig.onSetXDomain = (range) => {
+        const newRange = {start: new Date(range[0]), end: new Date(range[1])}
+        voltageConfig.xDomain = [newRange.start.getTime(), newRange.end.getTime()]
+        this.queryAndRender(this.buildQuery('voltage', newRange), this.loadedBMS, voltageConfig, 'Voltage', 'chart-voltage', this.loadedData.voltage)
+      }
+      voltageConfig.onResetXDomain = () => {
+        voltageConfig.xDomain = [this.range.start.getTime(), this.range.end.getTime()]
+        this.queryAndRender(this.buildQuery('voltage', this.range), this.loadedBMS, voltageConfig, 'Voltage', 'chart-voltage', this.loadedData.voltage)
+      }
 
-      this.queryAndRender(this.buildQueryCurrent(), this.selectedBMS, currentConfig, 'Current', 'chart-current', this.loadedData.current, 0)
-      this.queryAndRender(this.buildQueryVoltage(), this.selectedBMS, voltageConfig, 'Voltage', 'chart-voltage', this.loadedData.voltage)
-      this.queryAndRender(this.buildQueryTemperature(), this.selectedBMS, temperatureConfig, 'Temperature', 'chart-temperature', this.loadedData.temperature)
-
+      temperatureConfig.xDomain = null
+      //temperatureConfig.xDomain = [this.range.start.getTime(), this.range.end.getTime()]
+      temperatureConfig.onSetXDomain = (range) => {
+        const newRange = {start: new Date(range[0]), end: new Date(range[1])}
+        temperatureConfig.xDomain = [newRange.start.getTime(), newRange.end.getTime()]
+        this.queryAndRender(this.buildQuery('tempBatt', newRange), this.loadedBMS, temperatureConfig, 'Temperature', 'chart-temperature', this.loadedData.temperature)
+      }
+      temperatureConfig.onResetXDomain = () => {
+        temperatureConfig.xDomain = [this.range.start.getTime(), this.range.end.getTime()]
+        this.queryAndRender(this.buildQuery('tempBatt', this.range), this.loadedBMS, temperatureConfig, 'Temperature', 'chart-temperature', this.loadedData.temperature)
+      }
     },
     getBucket() {
       return this.logged ? adminBucket : bucket
     },
-    buildQueryCurrent() {
+    buildQuery(field, range) {
       return `from(bucket: "${this.getBucket()}") 
-              |> range(start: ${this.range.start.toISOString()}, stop: ${this.range.end.toISOString()})
+              |> range(start: ${range.start.toISOString()}, stop: ${range.end.toISOString()})
               |> filter(fn: (r) => r._measurement == "tlm")
-              |> filter(fn: (r) => r.bms == "${this.selectedBMS}")
-              |> filter(fn: (r) => (r._field == "current"))
-              |> group(columns: ["bms"])
+              |> filter(fn: (r) => r.bms == "${this.loadedBMS}")
+              |> filter(fn: (r) => (r._field == "${field}"))` +
+              (this.logged ? this.getAggregationWindow(range) : '') +
+             `|> group(columns: ["bms"])
               |> sort(columns: ["_time"])` 
     },
-    buildQueryVoltage() {
-      return `from(bucket: "${this.getBucket()}") 
-              |> range(start: ${this.range.start.toISOString()}, stop: ${this.range.end.toISOString()})
-              |> filter(fn: (r) => r._measurement == "tlm")
-              |> filter(fn: (r) => r.bms == "${this.selectedBMS}")
-              |> filter(fn: (r) => (r._field == "voltage"))
-              |> group(columns: ["bms"])
-              |> sort(columns: ["_time"])` 
-    },
-    buildQueryTemperature() {
-      return `from(bucket: "${this.getBucket()}") 
-              |> range(start: ${this.range.start.toISOString()}, stop: ${this.range.end.toISOString()})
-              |> filter(fn: (r) => r._measurement == "tlm")
-              |> filter(fn: (r) => r.bms == "${this.selectedBMS}")
-              |> filter(fn: (r) => (r._field == "tempBatt"))
-              |> group(columns: ["bms"])
-              |> sort(columns: ["_time"])` 
+    getAggregationWindow(range) {
+      var aw = this.computeAggregationWindowIntervalString(maxDataPoints, range.start, range.end)
+      //return `|> aggregateWindow(every: ${aw}, fn: last, createEmpty: false)`
+      return `|> aggregateWindow(every: ${aw}, fn: mean, createEmpty: false)`
     },
     startTimer() {
       this.timer = new Date()
