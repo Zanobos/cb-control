a24dca87ec0137c77b9ea746839307165997b447
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2021-04-07 18:15:10 +0200

    New token

--- control_node/cbdash-fe/src/config/env.js
+++ control_node/cbdash-fe/src/config/env.js
@@ -2,7 +2,7 @@
 //const url = process.env['INFLUX_URL'] || 'http://109.238.125.105:8086'
 const url = process.env['INFLUX_URL'] || 'http://localhost:8086'
 /** InfluxDB authorization token */
-const token = process.env['INFLUX_TOKEN'] || 'JTZDkRGHqj9YNB-dBypjBV9Ei9HQq-RumCA9yZrdRSM7M39j9-B0cplpbyjbMzzVphRPt-QZv3LGGTS3gbco9w=='
+const token = process.env['INFLUX_TOKEN'] || '3aUreoYqk6zPzfL2ryl-pqWZhIUVdaILWX6gLskL7uasgIJQSCTjySdLsGPbyOOv5nY4cN085tyWh8FBiSXt-Q=='
 /** Organization within InfluxDB  */
 const org = process.env['INFLUX_ORG'] || 'polar'
 /**InfluxDB bucket used in examples  */
